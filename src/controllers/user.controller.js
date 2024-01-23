bb4e82a5b5dd3a2cbf4d54d5e80180c34db88d70
unknown <64825962+D1urga@users.noreply.github.com>
2024-01-24 00:10:00 +0530

    filed added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,32 +4,40 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "something went wrong while generating token");
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
   if (
-    [fullName, email, username, password].some((field) => field?.trim() === "")
+    [fullName, email, password, username].some((field) => field?.trim() === "")
   ) {
-    throw new ApiError(400, "all fields are required");
+    throw new ApiError(400, "all fields required");
   }
-
-  const existeduser = User.findOne({ $or: [{ username }, { email }] });
-  if (existeduser) {
-    throw new ApiError(409, "user already exists");
+  const existedUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (existedUser) {
+    throw new ApiError(409, "user already exits");
   }
   const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
   if (!avatarLocalPath) {
     throw new ApiError(400, "avatar is required");
   }
-
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-
   if (!avatar) {
-    throw new ApiError(400, " avatar file is required");
+    throw new ApiError(400, "avatar file is required");
   }
-
   const user = await User.create({
     fullName,
     avatar: avatar.url,
@@ -38,18 +46,65 @@ const registerUser = asyncHandler(async (req, res) => {
     password,
     username: username.toLowerCase(),
   });
-
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
-
   if (!createdUser) {
-    throw new ApiError(500, "something went wrong while registration");
+    throw new ApiError(500, "something went wrong while registering");
   }
-
   res
     .status(201)
-    .json(new ApiResponse(200, createdUser, "registered user successfully"));
+    .json(new ApiResponse(200, createdUser, "user registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+
+  if (!username && !email) {
+    throw new ApiError(400, "username or email required");
+  }
+  const user = await User.findOne({ $or: [{ username }, { email }] });
+  if (!user) {
+    throw new ApiError(404, "user does not exits");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, {
+      expires: new Date(Date.now() + 30 * 24 * 3600000),
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .cookie("refreshToken", refreshToken, {
+      expires: new Date(Date.now() + 30 * 24 * 3600000),
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "user logged in successfully"
+      )
+    );
+});
+
+export { registerUser, loginUser };
