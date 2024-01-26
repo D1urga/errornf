78752841c7a40627046b5f8304f5e36ca53383ad
unknown <64825962+D1urga@users.noreply.github.com>
2024-01-26 17:01:31 +0530

    filed added

--- src/routes/contentPost.routes.js
+++ src/routes/contentPost.routes.js
@@ -4,12 +4,13 @@ import {
   postContentPost,
 } from "../controllers/post.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router
   .route("/postContentPost/:owner")
   .post(upload.fields([{ name: "content", maxCount: 1 }]), postContentPost);
-router.route("/getContentPost").get(getContentPost);
+router.route("/getContentPost").get(verifyJWT, getContentPost);
 
 export default router;
